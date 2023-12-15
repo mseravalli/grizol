0db423477d328c89bac43b7715c3a930789b444f
Marco Seravalli <marco.seravalli@gmail.com>
2023-12-15 20:39:50 +0000

    Add skeleton for an integration testing suite

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -11,6 +11,23 @@ function controlled_exit() {
   kill ${GRIZOL_PID} ${SYNCTHING_PID}
 }
 
+function run_diff() {
+  orig_dir=$1
+  dest_dir=$2
+  res=""
+  for f in $(ls ${orig_dir}); do
+    d=$(diff "${orig_dir}/${f}" "${dest_dir}/${f}" || true)
+    if [[ ! -z "$d" ]]; then
+      res="${f} ${res}"
+    fi
+  done
+  if [[ -z ${res} ]]; then 
+    echo "Success: No diffs found"
+  else
+    echo "Failures: Diffs found in ${res}"
+  fi
+}
+
 export RUST_BACKTRACE=1
 export RUSTFLAGS=-Awarnings
 export RUST_LOG=info,grizol=debug
@@ -36,9 +53,22 @@ tests/util/syncthing --home tests/util/syncthing_home > /tmp/syncthing 2>&1 &
 export SYNCTHING_PID=$!
 echo "started syncthing with pid ${SYNCTHING_PID}"
 
-sleep 4
+while [[ -z $(rg 'Ready to synchronize "orig_dir"' /tmp/syncthing) ]]; do sleep 1; done
 
+# 1: Test adding data
 scripts/create_random_files.sh tests/util/orig_dir/ 3
+while [[ $(rg 'Stored whole file' /tmp/grizol | wc -l) -ne 3 ]]; do sleep 1; done
+run_diff tests/util/orig_dir tests/util/dest_dir
+
+# 2: Test adding more data
+scripts/create_random_files.sh tests/util/orig_dir/ 3
+while [[ $(rg 'Stored whole file' /tmp/grizol | wc -l) -ne 6 ]]; do sleep 1; done
+run_diff tests/util/orig_dir tests/util/dest_dir
+
+# 3: Test modifying data
+file_name=$(ls tests/util/orig_dir/ | sort | head -n 1)
+head -c 100 "tests/util/orig_dir/${file_name}" > "tests/util/orig_dir/${file_name}" 
+run_diff tests/util/orig_dir tests/util/dest_dir
 
 # Ensure not to be waiting forever
 sleep 10000
