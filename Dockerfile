c7c4a8ca3e4171d6866af20dd576706e8127c57d
Marco Seravalli <marco.seravalli@gmail.com>
2024-02-15 21:54:34 +0000

    [wip] Add running example of docker container

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,14 @@ RUN mkdir -p /opt/grizol/config/
 RUN mkdir -p /opt/grizol/data/
 RUN mkdir -p /opt/grizol/state/
 
+ENV RUST_LOG=info
+
 EXPOSE 23456
 
 WORKDIR "/opt/grizol"
 
-
 CMD ["/opt/grizol/bin/grizol", "--config", "config/config.textproto"]
 
-COPY ./target/release/grizol /opt/grizol/bin/grizol
+COPY ./tests/util/rclone /usr/bin/rclone
 
+COPY ./target/release/grizol /opt/grizol/bin/grizol
--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -16,18 +16,23 @@ rclone_config: "config/rclone.conf"
 remote_base_dir: "seravalli-grizol"
 EOM
 
-CONFIG_PATH=$(mktemp)
+CONFIG_DIR=$(mktemp -d)
 
-echo "confing will be available at ${CONFIG_PATH}"
+echo "confing will be available at ${CONFIG_DIR}"
 
-echo "${CONFIG}" > ${CONFIG_PATH}
+echo "${CONFIG}" > ${CONFIG_DIR}/config.textproto
+
+export DATABASE_URL="sqlite:${CONFIG_DIR}/grizol.db"
+sqlx db reset -y
+sqlx migrate run
 
 docker run \
   --rm \
   --name grizol \
-  -it \
+  -d \
   -p 23456:23456 \
-  --volume=${CONFIG_PATH}:/opt/grizol/config/config.textproto \
+  --volume=${CONFIG_DIR}/config.textproto:/opt/grizol/config/config.textproto \
+  --volume=${CONFIG_DIR}/grizol.db:/opt/grizol/state/grizol.db \
   --volume=$PWD/tests/util/cert.pem:/opt/grizol/config/cert.pem \
   --volume=$PWD/tests/util/key.pem:/opt/grizol/config/key.pem \
   grizol/grizol:latest
