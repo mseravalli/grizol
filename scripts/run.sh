335a280c081f033c301522d94fe9ee027aa78803
Marco Seravalli <marco.seravalli@gmail.com>
2024-03-15 20:59:31 +0000

    Add minimal fuse subsystem

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,14 @@
 
 source ./scripts/env.sh
 
-cargo watch -x "check 2>&1 | tee /tmp/grizol_log"
-# cargo watch -x "run -- --config tests/util/config.textproto 2>&1 | tee /tmp/grizol_log"
+cmd=$1
+
+if [[ ${cmd} == "check" ]]; then 
+  cargo watch -x "check 2>&1 | tee /tmp/grizol_log"
+elif [[ ${cmd} == "run" ]]; then
+  cargo watch -x "run -- --config tests/util/config.textproto 2>&1 | tee /tmp/grizol_log"
+else
+  echo "Allowed values are only 'watch' and 'run', but '${cmd}' was provided instead"
+fi
+
+
